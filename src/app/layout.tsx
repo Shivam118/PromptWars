0cfa76eb7bb94f936b1758c4e430781cdd230284
Shivam118 <sharmashushant118@gmail.com>
2026-04-20 23:40:16 +0530

    A Bug Fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { EventProvider } from "@/store/EventContext";
 import { GoogleAnalytics } from "@next/third-parties/google";
